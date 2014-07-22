a295d2af01fa561507883ff84d2098a2a9ecccff
Sean Wolfe <sean3325@gmail.com>
2014-07-22 13:45:53 -0400

    make changes to project config

--- config.rb
+++ config.rb
@@ -8,11 +8,16 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
+# Require any additional compass plugins installed on your system.
+require 'sassy-buttons'
+require 'breakpoint'
+require "rgbapng"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
