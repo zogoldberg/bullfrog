903eb9b4889ec424fa1c73ab90dde1e16070af68
Zoe Goldberg <zoegoldberg@Zoe-Goldbergs-MacBook-Pro.local>
2014-07-30 20:33:28 -0400

    final tabs and styling

--- scripts/bullfrog.js
+++ scripts/bullfrog.js
@@ -2,12 +2,13 @@ $(document).ready(function(){
 
 	$('section.content-right article').hide();
 
-	$('section.content-center h2').click(function() {
+
+	$('section.content-center #related').click(function() {
 		$('section.content-center ul').show();
 		$('section.content-right article').hide();
 	});
 
-	$('section.content-right h2').click(function() {
+	$('section.content-center #featured').click(function() {
 		$('section.content-center ul').hide();
 		$('section.content-center a.read-more').hide();
 		$('section.content-right article').show();
